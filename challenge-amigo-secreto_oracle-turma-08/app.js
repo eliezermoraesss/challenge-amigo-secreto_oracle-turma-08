28a7f8cc3643805d5c000db1cd6b7fcb099bd9bd
Eliezer Moraes Silva <eliezer.moraes@outlook.com>
2025-01-15 20:13:02 -0300

    :sparkles: feat: definição e implementação da função sortearAmigos() finalizada

--- challenge-amigo-secreto_oracle-turma-08/app.js
+++ challenge-amigo-secreto_oracle-turma-08/app.js
@@ -1,10 +1,10 @@
 // Array para armazenar os amigos
-let amigos = []
+let amigos = [];
 
 // Função par adicionar amigo
 function adicionarAmigo() {
     const input = document.getElementById("amigo");
-    const nomeAmigo = input.ariaValueMax.trim(); // Remove os espaços extras (ou indesejados)
+    const nomeAmigo = input.value.trim(); // Remove os espaços extras (ou indesejados)
 
     // Validação do campo
     if (!nomeAmigo) {
@@ -35,4 +35,28 @@ function atualizarListaAmigos() {
         li.textContent = `${index + 1}. ${amigo}`; // Adiciona o índice e o nome do amigo
         listaAmigos.appendChild(li);
     });
+}
+
+function sortearAmigo() {
+    const resultado = document.getElementById("resultado");
+
+    // Limpa o resultado anterior
+    resultado.innerHTML = "";
+
+    // Valida se há amigos no array
+    if (amigos.length === 0) {
+        alert("A lista de amigos está vazia. Adicione amigos antes de sortear.");
+        return;
+    }
+
+    // Gera um índice aleatório
+    const indiceSorteado = Math.floor(Math.random() * amigos.length);
+
+    // Armazenar o nome sorteado
+    const amigoSorteado = amigos[indiceSorteado];
+
+    // Exibe o resultado na tela
+    const li = document.createElement("li");
+    li.textContent = `Amigo sorteado: ${amigoSorteado}`;
+    resultado.appendChild(li)
 }
